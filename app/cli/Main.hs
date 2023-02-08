adc5f14da044ed220251645836b347437c5be2c6
Alberto Fanton <alberto.fanton@protonmail.com>
2023-02-08 01:44:57 +0100

    Add limit support to cli

--- app/cli/Main.hs
+++ app/cli/Main.hs
@@ -2,7 +2,7 @@
 
 module Main (main) where
 
-import Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy as BL
 import Data.Foldable (traverse_)
 import Data.Semigroup ((<>))
 import Domain (
@@ -14,6 +14,8 @@ import Domain (
 import Options.Applicative
 import System.Random
 
+type Limit = Int
+
 randomO :: Parser Operation
 randomO = flag' Random (long "random" <> help "Get a list with only one random book")
 listO :: Parser Operation
@@ -22,22 +24,23 @@ listO = flag' List (long "list" <> help "Get the same list you passed")
 operation :: Parser Operation
 operation = randomO <|> listO
 
-params :: Parser (FilePath, FilePath, Operation)
+params :: Parser (FilePath, FilePath, Limit, Operation)
 params =
-    (,,)
+    (,,,)
         <$> argument str (metavar "INPUT_BOOKS_FILE")
         <*> argument str (metavar "OUTPUT_BOOKS_FILE")
+        <*> option auto (long "limit" <> short 'l' <> metavar "LIMIT" <> help "The number of books")
         <*> operation
 
-opts :: ParserInfo (FilePath, FilePath, Operation)
+opts :: ParserInfo (FilePath, FilePath, Limit, Operation)
 opts = info (params <**> helper) (fullDesc <> progDesc "Do something with you good read books" <> header "BetterReads - A toolkit to better handle goodread books")
 
 main :: IO ()
 main = do
-    (inputFilePath, outputFilePath, op) <- execParser opts
+    (inputFilePath, outputFilePath, limit, op) <- execParser opts
     csvData <- BL.readFile inputFilePath
     randomGen <- initStdGen
     case parseBooks csvData of
         Left err -> putStrLn err
         Right (_, toList -> books) ->
-            BL.writeFile outputFilePath $ encodeBooks (apply op randomGen books)
+            BL.writeFile outputFilePath $ encodeBooks $ take limit (apply op randomGen books)
