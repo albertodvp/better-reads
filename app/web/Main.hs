3db8672ccd3d78f8ab5cac0008895fa37872caed
Alberto Fanton <alberto.fanton@protonmail.com>
2023-02-12 01:06:23 +0100

    Revert "TMP working nix version"
    This reverts commit 89104b16cbc447ded2b1e1e4b6e8666ab4b5b014.

--- app/web/Main.hs
+++ app/web/Main.hs
@@ -1,21 +1,16 @@
 module Main (main) where
 
 import App (app)
-import qualified Network.Wai as Wai
 import Network.Wai.Handler.Warp (run)
-import qualified Network.Wai.Handler.Warp as Wai
-import qualified Network.Wai.Middleware.Gzip as Wai
-import qualified Network.Wai.Middleware.RequestLogger as Wai
-import qualified System.IO as IO
-
-oldMain :: IO ()
-oldMain = run 8081 app
 
 main :: IO ()
-main = do
-    IO.hPutStrLn IO.stderr $ "Running on port " ++ show port
-    Wai.run port $ Wai.logStdout $ compress app
-  where
-    port = 3003
-    compress :: Wai.Middleware
-    compress = Wai.gzip Wai.def{Wai.gzipFiles = Wai.GzipCompress}
+main = run 8081 app
+
+--
+
+-- main :: IO ()
+-- main = do
+--   let port = 3003
+--   IO.hPutStrLn IO.stderr $ "Running on port " ++ show port
+
+--   Wai.run
--- src/App.hs
+++ src/App.hs
@@ -1,82 +1,82 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
 
 module App (app) where
 
-import Data.Proxy
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Lazy as B
+import Data.Foldable (toList)
+import qualified Data.Text as T
+import Domain (
+    Book (..),
+    Operation (..),
+    apply,
+    encodeBooks,
+    parseBooks,
+ )
+import Servant
+import System.Random
 
--- import           Control.Monad.IO.Class (liftIO)
--- import           Domain                 (Book (..), Operation (..), apply,
---                                          encodeBooks, parseBooks)
+data PingMode = Loud | Normal
 
-import Common
-import qualified Lucid as L
-import qualified Network.HTTP.Types as HTTP
-import qualified Network.Wai as Wai
-import qualified Network.Wai.Handler.Warp as Wai
-import qualified Network.Wai.Middleware.Gzip as Wai
-import qualified Network.Wai.Middleware.RequestLogger as Wai
-import Servant ((:<|>) (..), (:>))
-import qualified Servant
+capitalize :: T.Text -> T.Text
+capitalize = T.toTitle . T.toLower
 
--- TODO: check types
--- capitalize :: T.Text -> T.Text
--- capitalize = T.toTitle . T.toLower
+paramErr :: T.Text
+paramErr = "Invalid param"
 
--- paramErr :: T.Text
--- paramErr = "Invalid param"
+newtype ParsedOperation = ParsedOperation Operation
 
--- newtype ParsedOperation = ParsedOperation Operation
+instance FromHttpApiData ParsedOperation where
+    parseUrlPiece = f . capitalize
+      where
+        f "Random" = Right (ParsedOperation Random)
+        f "List" = Right (ParsedOperation List)
+        f _ = Left paramErr
 
--- instance FromHttpApiData ParsedOperation where
---     parseUrlPiece = f . capitalize
---       where
---         f "Random" = Right (ParsedOperation Random)
---         f "List"   = Right (ParsedOperation List)
---         f _        = Left paramErr
-
--- instance FromHttpApiData PingMode where
---     parseQueryParam = f . capitalize
---       where
---         f "Loud"   = Right Loud
---         f "Normal" = Right Normal
---         f _        = Left paramErr
+instance FromHttpApiData PingMode where
+    parseQueryParam = f . capitalize
+      where
+        f "Loud" = Right Loud
+        f "Normal" = Right Normal
+        f _ = Left paramErr
 
 -- TODO: what is happening here?
---       - what is '[OctetStream] (DataKinds)
+--       - what is '[JSON] (DataKinds)
 --       - does (:<|>), (:>) work (TypeOperators)
--- type BookOperationAPI = "booksOperation"
---                         :> Capture "operation" ParsedOperation
---                         :> QueryParam "limit" Int
---                         :> ReqBody '[OctetStream] B.ByteString
---                         :> Post '[OctetStream] B.ByteString
-
 type API =
-    -- StaticAPI
-    -- :<|> BookOperationAPI
-    -- :<|> ServerRoutes
-    -- :<|> Servant.Raw
-    Servant.Raw
+    Get '[JSON] String
+        :<|> "ping" :> QueryParam "mode" PingMode :> Get '[JSON] String
+        -- TODO: implement this by yourself
+        :<|> "booksOperation" :> Capture "operation" ParsedOperation :> QueryParam "limit" Int :> ReqBody '[OctetStream] B.ByteString :> Post '[OctetStream] B.ByteString
 
--- applyOperation :: Operation -> [Book] -> Handler [Book]
--- applyOperation op books = do
---     gen <- liftIO initStdGen
---     pure $ apply op gen books
+handlerPingPong :: Maybe PingMode -> Handler String
+handlerPingPong mm =
+    let res = case mm of
+            Just Loud -> "PONG"
+            Just Normal -> "pong"
+            Nothing -> "..."
+     in pure res
 
--- booksOperationHandler :: ParsedOperation -> Maybe Int -> B.ByteString -> Handler B.ByteString
--- booksOperationHandler pOp Nothing booksBS = booksOperationHandler pOp (Just 1) booksBS
--- booksOperationHandler (ParsedOperation op) (Just n) booksBS = case parseBooks booksBS of
---     Left _ -> throwError $ err400{errBody = "Bad request :("}
---     Right (_, books) -> encodeBooks . take n <$> applyOperation op (toList books)
-app :: Wai.Application
-app =
-    Servant.serve (Proxy @API) (Servant.Tagged page404)
-  where
-    page404 :: Wai.Application
-    page404 _ respond =
-        respond
-            $ Wai.responseLBS
-                HTTP.status404
-                [("Content-Type", "text/plain")]
-            $ L.renderBS
-            $ L.toHtml Common.page404View
+applyOperation :: Operation -> [Book] -> Handler [Book]
+applyOperation op books = do
+    gen <- liftIO initStdGen
+    pure $ apply op gen books
+
+booksOperationHandler :: ParsedOperation -> Maybe Int -> B.ByteString -> Handler B.ByteString
+booksOperationHandler pOp Nothing booksBS = booksOperationHandler pOp (Just 1) booksBS
+booksOperationHandler (ParsedOperation op) (Just n) booksBS = case parseBooks booksBS of
+    Left _ -> throwError $ err400{errBody = "Bad request :("}
+    Right (_, books) -> encodeBooks . take n <$> applyOperation op (toList books)
+
+server :: Server API
+server =
+    return "Hello, servant!"
+        :<|> handlerPingPong
+        :<|> booksOperationHandler
+
+rootApi :: Proxy API
+rootApi = Proxy
+
+app :: Application
+app = serve rootApi server
