405f6e76e2a38e73d7e0836ac3b23a553deb9f55
Alberto Fanton <alberto.fanton@protonmail.com>
2023-03-01 23:43:51 +0100

    Pulumi exps

--- app/web/Main.hs
+++ app/web/Main.hs
@@ -1,12 +1,12 @@
 module Main (main) where
 
-import App (app)
-import Network.Wai.Handler.Warp (run)
-import Network.Wai.Middleware.RequestLogger (logStdout)
-import System.IO (hPutStrLn, stderr)
+import           App                                  (app)
+import           Network.Wai.Handler.Warp             (run)
+import           Network.Wai.Middleware.RequestLogger (logStdout)
+import           System.IO                            (hPutStrLn, stderr)
 
 main :: IO ()
 main = do
-    let port = 3003
+    let port = 80
     hPutStrLn stderr $ "Running on port " ++ show port
     run port $ logStdout app
